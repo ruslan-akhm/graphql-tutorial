39188285c99e19e270473faaa129db2a678f28e1
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-06-05 15:38:27 -0400

    graphql basic setup

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => {
+          return "Hello World";
+        },
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => {
   console.log("Listenning on 5000");
